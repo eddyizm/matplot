a31ec9267d2d130b2221c57d1a69e95ea36aa7e3
Eduardo Cervantes <eddyizm@gmail.com>
2018-05-18 16:53:33 -0700

    working on big stack graph

--- recreate.py
+++ recreate.py
@@ -22,6 +22,7 @@ ax.set_ylabel('Test Average')
 plt.savefig('my_bar_chart.png')
 plt.show()
 '''
+'''
 # side by side bars
 from matplotlib import pyplot as plt
 
@@ -46,4 +47,24 @@ plt.title('Test Averages on Different Units')
 ax.set_xlabel('Unit')
 ax.set_ylabel('Test Average')
 plt.savefig('my_side_by_side.png')
+plt.show()
+'''
+from matplotlib import pyplot as plt
+import numpy as np
+
+unit_topics = ['Limits', 'Derivatives', 'Integrals', 'Diff Eq', 'Applications']
+As = [6, 3, 4, 3, 5]
+Bs = [8, 12, 8, 9, 10]
+Cs = [13, 12, 15, 13, 14]
+Ds = [2, 3, 3, 2, 1]
+Fs = [1, 0, 0, 3, 0]
+
+x = range(5)
+
+c_bottom = np.add(As, Bs)
+#create d_bottom and f_bottom here
+d_bottom = np.add(Cs, c_bottom)
+f_bottom = np.add(Ds, d_bottom)
+#create your plot here
+
 plt.show()
