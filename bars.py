106b15d709bfb6e0ea9db955cf2138605cae9eeb
Eduardo Cervantes <eddyizm@gmail.com>
2018-05-18 13:19:39 -0700

    added error bar lesson

--- bars.py
+++ bars.py
@@ -8,8 +8,8 @@ plt.bar(range(len(drinks)), sales)
 #create your ax object here
 ax = plt.subplot()
 plt.show()
-'''
-# 4/12 
+
+# 4/12 double bars side by side
 from matplotlib import pyplot as plt
 
 drinks = ["cappuccino", "latte", "chai", "americano", "mocha", "espresso"]
@@ -35,5 +35,31 @@ store2_x = [t*element + w*n for element
              in range(d)]
 
 plt.bar(store2_x, sales2)
+
+plt.show()
+
+# 5/12 Stacked Bars
+from matplotlib import pyplot as plt
+
+drinks = ["cappuccino", "latte", "chai", "americano", "mocha", "espresso"]
+sales1 =  [91, 76, 56, 66, 52, 27]
+sales2 = [65, 82, 36, 68, 38, 40]
+plt.bar(range(len(sales1)), sales1)
+plt.bar(range(len(sales2)), sales2, bottom=sales1)
+legend_labels = ['Location 1', 'Location 2']
+plt.legend(legend_labels)
+plt.show()
+'''
+
+# 6/12 Error Bars
+from matplotlib import pyplot as plt
+
+drinks = ["cappuccino", "latte", "chai", "americano", "mocha", "espresso"]
+ounces_of_milk = [6, 9, 4, 0, 9, 0]
+error = [0.6, 0.9, 0.4, 0, 0.9, 0]
+
+# Plot the bar graph here
+plt.bar(range(len(ounces_of_milk)), ounces_of_milk, yerr=error, capsize=5)
+
 
 plt.show()
